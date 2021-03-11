3f99382e588fb6f837d13a76646e25e88132dae0
fahrul-rozi <fahrul362@gmail.com>
2021-03-11 20:23:49 +0700

    project_cover

---
+++ index.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PPDB2021SALUT</title>
+    <link href="https://fonts.googleapis.com/css2?family=Faster+One&family=Racing+Sans+One&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <link rel="stylesheet" href="assets/responsive.css">
+</head>
+
+<body>
+    <nav>
+        <div class="logo">
+            <img src="assets/img/logo.png" alt="logo" ;">
+            <!-- <h4>SMK Negeri 1 Pemulutan</h4> -->
+        </div>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="#our">Jurusan</a></li>
+            <li><a href="#review">Pelaksanaan</a></li>
+            <li><a href="#about">About Us</a></li>
+        </ul>
+        <div class="menu-toggle">
+            <input type="checkbox">
+            <span class="menu-toggle span"></span>
+            <span></span>
+            <span></span>
+        </div>
+
+    </nav>
+    <section>
+        <div class="header">
+            <div class="top-wrapper" id="home">
+                <h1>SELAMAT DATANG DI WEBSITE PPDB2021</h1>
+                <h2>SMK NEGERI 1 PEMULUTAN</h2>
+                <div class="text">
+                    <!--<p>Kami beranggotakan 5 orang yang berasal dari berbagai daerah di Indonesia, ada yang berasal dari Pulau Sumatera, Kalimantan dan Sulawesi</p>
+                    <p>Di dalam Tim F1 ada M. Haidir Hasan, Miftakhul Huda, M. Dico Triyadi, Mega Lestari dan Mareta Siregar</p>-->
+                </div>
+                <div class="btn-wrapper">
+                    <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSeNMTzCQ0WTS4zsHX8bRezPyRKUBcaUGGJecAy2ofnTonvvLA/viewform?embedded=true" class="btn">Daftar Sekarang !</a>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+
+    <div class="about" id="about">
+        <!--<h1>About Us</h1>-->
+        <img src="assets/img/logo-smk-black.png" alt="logof1">
+        <div class="paragrap">
+            <h3>TERAKREDITAS "B"</h3>
+            <p>"SMK Negeri 1 Pemulutan merupakan SMK Negeri yang berada wilayah kecamatan Pemulutan kabupaten Ogan Ilir didirikan pada tahun 2009. SMK ini mengarah pada bidang keahlian Bisnis dan Manajemen"</p>
+            <br>
+            <h3>VISI</h3>
+            <p>Menjadi Pusat Pendidikan yang Unggul dan Mampu Menghasilkan Lulusan Tingkat Menengah yang Berstandar Nasional, Berwawasan Lingkungan, Berkarakter Bangsa, Bersaing di Era Global, Berlandasan IMTAQ. </p>
+        </div>
+    </div>
+
+    <!-- Jurusan -->
+    <div class="team-section" id="our">
+        <div class="person">
+            <img src="assets/img/AKL.jpg" alt="" class="person-pic">
+            <div class="person-info">
+                <h2>Akuntansi dan Keuangan Lembaga</h2>
+                <p>Mampu melakukan perhitungan keuangan sampai pada proses pelaporan</p>
+                
+            </div>
+        </div>
+
+        <div class="person">
+            <img src="assets/img/OTKP.jpg" alt="" class="person-pic">
+            <div class="person-info">
+                <h2>Otomatisasi dan Tata Kelola Perkantoran</h2>
+                <p>Memahami K3 serta mengadminstrasi tata kelola perkantoran</p>
+                
+            </div>
+        </div>
+
+        <div class="person">
+            <img src="assets/img/BDP.jpg" alt="" class="person-pic">
+            <div class="person-info">
+                <h2>Bisnis Daring dan Pemasaran</h2>
+                <p>Mampu membuka usaha dan membangun hubungan dengan konsumen</p>
+                
+            </div>
+        </div>
+    </div>
+
+<!-- waktu Pelaksanaa-->
+    <div class="slide">
+        <div class="slide-konten" id="review">
+            <div class="oii">
+                <div class="slide-img"> <img src="assets/img/p6.png"></div>
+                <div class="slide-konten-text">
+                    <H3>Pendaftaran</H3>
+                    <p>11-14 Mei 2021</p>
+                </div>
+            </div>
+            <div class="oii">
+                <div class="slide-img"> <img src="assets/img/p7.png"></div>
+                <div class="slide-konten-text">
+                    <H3>Pengumuman Hasil</H3>
+                    <p>09 Juni 2021</p>
+                </div>
+            </div>
+            <div class="oii">
+                <div class="slide-img"> <img src="assets/img/p8.png"></div>
+                <div class="slide-konten-text">
+                    <h3>Verifikasi dan Validasi Berkas</h3>
+                    <p>11-13 Juni 2021</p>
+                </div>
+            </div>
+            <div class="oii">
+                <div class="slide-img"> <img src="assets/img/p9.png"></div>
+                <div class="slide-konten-text">
+                    <h3>Tes Bakat Minat</h3>
+                    <p>24 Juni 2021</p>
+                </div>
+            </div>
+            <div class="oii">
+                <div class="slide-img"> <img src="assets/img/p10.png"></div>
+                <div class="slide-konten-text">
+                    <h3>Pengumuman hasil Seleksi</h3>
+                    <p>27 Juni 2021</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+    <!-- footer -->
+    <footer class="footer">
+        <div class="footer__addr">
+            <a href="http://smknsalut.sch.id"><h1 class="footer__logo"><img src="assets/img/logo.png"></h1></a>
+            <h2>Contact</h2>
+            
+
+            <address>
+                <a class="footer__btn" href="http://smknsalut.sch.id/contact">SMKSALUT</a>
+            </address>
+        </div>
+
+        <ul class="footer__nav">
+            <li class="nav__item">
+                <h2 class="nav__title">Home</h2>
+                <ul class="nav__ul">
+                    <li>
+                        <a href="#">Privacy Policy</a>
+                    </li>
+                    <li>
+                        <a href="#">Terms of Use</a>
+                    </li>
+                    <li>
+                        <a href="#">Sitemap</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="nav__item">
+                <h2 class="nav__title">Panitia</h2>
+                <ul class="nav__ul">
+                    <li>
+                        <a href="#our">Bpk. Usmi Sintara</a>
+                    </li>
+                    <li>
+                        <a href="#our">Ibu. Yuliarti Triana</a>
+                    </li>
+                    <li>
+                        <a href="#our">Bpk. Mus Mulyadi</a>
+                    </li>
+                    <li>
+                        <a href="#our">Bpk. Usmi Sintara</a>
+                    </li>
+                    <li>
+                        <a href="#our">Bpk. Usmi Sintara</a>
+                    </li>
+
+                </ul>
+            </li>
+
+            <li class="nav__item">
+                <h2 class="nav__title"></h2><br>
+                <ul class="nav__ul">
+                    <li>
+                        <a href="#our">Bpk. Usmi Sintara</a>
+                    </li>
+                    <li>
+                        <a href="#our">Bpk. Usmi Sintara</a>
+                    </li>
+                    <li>
+                        <a href="#our">Bpk. Usmi Sintara</a>
+                    </li>
+                    <li>
+                        <a href="#our">Bpk. Usmi Sintara</a>
+                    </li>
+                    <li>
+                        <a href="#our">Bpk. Usmi Sintara</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+
+        <div class="legal">
+            <p>2021 &copy; PPDB2021-SMKNSALUT.All rights reserved.</p>
+
+            <div class="legal__links">
+
+            </div>
+        </div>
+    </footer>
+
+    <script src="assets/script.js"></script>
+</body>
+
+</html>
